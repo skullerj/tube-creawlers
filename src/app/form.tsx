592b1f9da419ddf9de98f9da8ae9d3e70eb3d0a3
Juan Rosero <juan@rosero.dev>
2024-08-01 21:29:04 +0100

    feat: improve layout and add form validation

--- src/app/form.tsx
+++ src/app/form.tsx
@@ -11,27 +11,44 @@ import {
 	Form,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
+import { z } from "zod";
 
+const schema = z.object({
+	name: z.string().min(2, "Needs to have at least 2 characters"),
+});
 export function RouteForm() {
-	const form = useForm();
+	const form = useForm({
+		resolver: zodResolver(schema),
+		defaultValues: {
+			name: "",
+		},
+	});
 	return (
 		<Form {...form}>
-			<FormField
-				name="name"
-				control={form.control}
-				render={({ field }) => (
-					<FormItem>
-						<FormLabel>Route Name</FormLabel>
-						<FormControl>
-							<Input {...field} placeholder="name" />
-						</FormControl>
-						<FormDescription>This is the name for your route.</FormDescription>
-						<FormMessage />
-					</FormItem>
-				)}
-			/>
-			<Button>Clicks can now be done</Button>
+			<form
+				className="w-full max-w-lg flex flex-col gap-2"
+				onSubmit={form.handleSubmit((data) => console.log(data))}
+			>
+				<FormField
+					name="name"
+					control={form.control}
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>Route Name</FormLabel>
+							<FormControl>
+								<Input {...field} placeholder="name" />
+							</FormControl>
+							<FormDescription>
+								This is the name for your route.
+							</FormDescription>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+				<Button type="submit">Create Route</Button>
+			</form>
 		</Form>
 	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import { RouteForm } from "./form";
 export default function Home() {
 	return (
-		<main className="flex w-100 flex-col">
+		<main className="flex w-screen h-screen flex-col items-center p-4">
+			<h1>Create new route</h1>
 			<RouteForm />
 		</main>
 	);

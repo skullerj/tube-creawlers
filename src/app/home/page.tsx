fe2291c8441841c3c273a1ef3f218487bb596c03
Juan Rosero <juan@rosero.dev>
2024-09-26 22:20:37 +0100

    feat: users now have a protected home behind a loging wall

---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+	return (
+		<main>
+			<h1>
+				well... now you're in. This is kinda akward since there is nothing here
+				yet... soo... yeah... thanks for making it this far though... it's
+				really sweet of you, you are a great person with a wonderful
+				personality, I can tell just by how you clicked that last button
+			</h1>
+		</main>
+	);
+}
--- src/app/nav.tsx
+++ src/app/nav.tsx
@@ -11,14 +11,10 @@ export default function Nav() {
 		<nav className="h-12 w-full flex items-center px-4 drop-shadow-sm bg-card text-card-foreground border-b">
 			<h1 className="text-lg flex-1">Tube Crawlers</h1>
 			<div className="flex gap-2">
-				<Link href="/" className={pathname === "/" ? "underline" : ""}>
+				<Link href="/home" className={pathname === "/home" ? "underline" : ""}>
 					Home
 				</Link>
-				{user ? (
-					<a href="/api/auth/logout">Log Out</a>
-				) : (
-					<a href="/api/auth/login">Log In</a>
-				)}
+				{!user && <a href="/api/auth/logout">Log Out</a>}
 			</div>
 		</nav>
 	);
